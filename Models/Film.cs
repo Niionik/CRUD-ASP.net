24c58b70150ca9c2e937cc3f3086d75f66854b7a
Nionik <Patrycja.pl.opalacz@gmail.com>
2024-11-14 20:30:42 +0100

    Dodaj pliki projektów.

---
+++ Models/Film.cs
@@ -0,0 +1,10 @@
+namespace CRUD.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+} 
